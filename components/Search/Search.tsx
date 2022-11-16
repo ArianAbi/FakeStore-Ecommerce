269f99a1dca49f3e416e0209eb606ae0c17147a2
arian <kapqworks@gmail.com>
2022-11-16 15:48:00 +0330

    Update Header Search is now responssive for mobile devices

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import SearchIcon from '@mui/icons-material/Search'
+import CloseIcon from '@mui/icons-material/Close';
+import styles from '/styles/layout/Search.module.css';
 
 interface Search {
     term: string | string[] | undefined,
@@ -7,17 +10,38 @@ interface Search {
 }
 
 export const Search = ({ term, setTerm, onSubmit }: Search) => {
+
+    const [open, setOpen] = useState(false);
+
+    const toggleSearch = () => {
+        setOpen(!open)
+    }
+
     return (
-        <form onSubmit={onSubmit}>
-            <input
-                className="w-[200px] h-[25px] left-0 right-0 top-0 bottom-0 px-4 rounded-full text-black
-                md:absolute md:mx-auto md:my-auto md:w-[300px]
-                lg:w-[400px]
-                "
-                placeholder='search term'
-                value={term}
-                onChange={e => setTerm(e.target.value.toLowerCase())}
-            />
-        </form>
+        <>
+
+            <button className='z-10 text-black' onClick={toggleSearch}>
+                <SearchIcon sx={{ fill: open ? "black" : "white" }} />
+            </button>
+
+            <form
+                onSubmit={onSubmit}
+                className={`${open ? styles.formActive : styles.formDeactive}`}
+            >
+                <input
+                    className={`${open ? styles.active : styles.deactive} text-black`}
+                    placeholder='search term'
+                    value={term}
+                    onChange={e => setTerm(e.target.value.toLowerCase())}
+                />
+
+                <div
+                    onClick={toggleSearch}
+                >
+                    <CloseIcon />
+                </div>
+            </form>
+
+        </>
     )
 }
