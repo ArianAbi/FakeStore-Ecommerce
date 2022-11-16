7e646e6098aeba71bffc27e5a4a61684ca9f64d8
arian <kapqworks@gmail.com>
2022-11-16 11:36:10 +0330

    Update LandingPage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,7 @@
 import Link from "next/link"
 import Head from "next/head";
-import { useEffect } from "react"
-import Router from "next/router"
 
 export default function LandingPage() {
-  useEffect(() => {
-    Router.replace("/products")
-  }, [])
 
   return (
     <>
@@ -16,7 +11,7 @@ export default function LandingPage() {
 
       <div className="h-screen w-screen flex flex-col items-center justify-center gap-4 bg-black">
         <h2 className="text-4xl">
-          Landing Page
+          Landing Page Updated
         </h2>
         <Link
           className="text-cyan-400 text-lg underline"
