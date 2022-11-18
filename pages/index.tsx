a0675b0da1e3a4d7857cd489e87c66ec6d0959f4
arian <kapqworks@gmail.com>
2022-11-18 13:15:21 +0330

    Add landing page first section desine (mobile only)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import Head from "next/head";
+import styles from '/styles/landing.module.css';
 
 export default function LandingPage() {
 
@@ -9,7 +10,7 @@ export default function LandingPage() {
         <title>HomePage</title>
       </Head>
 
-      <div className="h-screen w-full  flex flex-col items-center justify-center gap-4 bg-black">
+      {/* <div className="h-screen w-full  flex flex-col items-center justify-center gap-4 bg-black">
         <h2 className="text-4xl">
           Still in Development
         </h2>
@@ -19,6 +20,48 @@ export default function LandingPage() {
         >
           visit /products or click on this link
         </Link>
+      </div> */}
+
+      <div className="flex flex-col justify-start items-center pt-6 w-full min-h-screen">
+
+        <h2 className="text-xl text-black font-bold mb-4 md:text-2xl lg:text-3xl">Welcome to FakeStore</h2>
+
+        <div className={styles.container}>
+          <Link
+            href="/products?category=men%27s+clothing"
+            className={`${styles.category_item} bg-[url('/landing_page/men.webp')]`}
+          >
+            Mens Clothing
+          </Link>
+
+          <Link
+            href="/products?category=women%27s+clothing"
+            className={`${styles.category_item} bg-[url('/landing_page/women.webp')]`}
+          >
+            Womens Clothing
+          </Link>
+
+          <Link
+            href="/products?category=electronics"
+            className={`${styles.category_item} bg-[url('/landing_page/electronics.webp')]`}
+          >
+            Electronics
+          </Link>
+
+          <Link
+            href="/products?category=jewelery"
+            className={`${styles.category_item} bg-[url('/landing_page/jewelery.webp')]`}
+          >
+            Jewelery
+          </Link>
+
+          <Link
+            href="/products"
+            className={`${styles.all_category_item} bg-[url('/landing_page/all.webp')]`}
+          >
+            All Products
+          </Link>
+        </div>
       </div>
     </>
   )
