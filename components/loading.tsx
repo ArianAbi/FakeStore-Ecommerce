85ab14d37a43da4d6d3c24f9adb4df83e4be6b5b
arian <kapqworks@gmail.com>
2023-08-15 21:17:48 +0330

    Bugfix : make loading position fixed

--- components/loading.tsx
+++ components/loading.tsx
@@ -2,7 +2,7 @@ import styles from '/styles/loading.module.css';
 
 export const Loading = () => {
     return (
-        <div className="absolute flex flex-col items-center justify-center w-full h-screen bg-black bg-opacity-60 z-[9999] text-white">
+        <div className="fixed top-0 left-0 flex flex-col items-center justify-center w-screen h-screen bg-black bg-opacity-60 z-[9999] text-white">
             <div className="animate-spin">
                 <div className="w-[45px] h-[45px] border-4 rounded-lg"></div>
             </div>
